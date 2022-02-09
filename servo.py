26691557c59665af422f45b3e8341d12b80cee60
Justin <jfeng10@g.ucla.edu>
2022-02-08 21:27:18 -0800

    servo.py

---
+++ servo.py
@@ -0,0 +1,47 @@
+# Use Raspberry Pi to control Servo Motor motion
+# Tutorial URL: http://osoyoo.com/?p=937
+
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setwarnings(False)
+# Set the layout for the pin declaration
+GPIO.setmode(GPIO.BOARD)
+# The Raspberry Pi pin 11(GPIO 18) connect to servo signal line(yellow wire)
+# Pin 11 send PWM signal to control servo motion
+GPIO.setup(11, GPIO.OUT)
+
+# menu info
+print("s: move one step")
+position = 2.5
+direction = 1
+
+while True:
+	# Now we will start with a PWM signal at 50Hz at pin 18. 
+	# 50Hz should work for many servos very will. If not you can play with the frequency if you like.
+	Servo = GPIO.PWM(11, 50)						
+
+	# This command sets the left position of the servo
+	Servo.start(2.5)
+
+	# Now the program asks for the direction the servo should turn.
+	input = raw_input("Selection: ") 
+
+	# You can play with the values.
+	# 7.5 is in most cases the middle position
+	# 12.5 is the value for a 180 degree move to the right
+	# 2.5 is the value for a -90 degree move to the left
+	if(input == "s"):
+		if (direction == 1 and position < 12.5):
+			position = position + 2.5
+			Servo.ChangeDutyCycle(position)
+			if (position == 12.5):
+				direction = 0
+
+		elif (direction == 0 and position > 2.5):
+			position = position - 2.5
+			Servo.ChangeDutyCycle(position)
+			if (position == 2.5):
+				direction = 1
+			
