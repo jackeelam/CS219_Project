f1a11335abbbd05d5280868970dcfc1b650edb46
Chester <chester.h4@gmail.com>
2022-02-15 19:21:02 -0800

    working

--- gear_motor.py
+++ gear_motor.py
@@ -6,26 +6,44 @@ GPIO.setmode(GPIO.BOARD)
 Motor1 = 16    # Input Pin
 Motor2 = 18    # Input Pin
 Motor3 = 22    # Enable Pin
- 
+
 GPIO.setup(Motor1,GPIO.OUT)
 GPIO.setup(Motor2,GPIO.OUT)
 GPIO.setup(Motor3,GPIO.OUT)
- 
-print "FORWARD MOTION"
+
+p = GPIO.PWM(Motor3,100)          #GPIO19 as PWM output, with 100Hz frequency
+
+p.start(10)
+GPIO.output(Motor1,GPIO.HIGH)
+GPIO.output(Motor2,GPIO.LOW)
+sleep(5)
+
+'''
+p.start(0)
+
+for x in range (50):                          #execute loop for 50 times, x being incremented from 0 to 49.
+    GPIO.output(Motor1,GPIO.HIGH)
+    GPIO.output(Motor2,GPIO.LOW)
+    p.ChangeDutyCycle(x)               #change duty cycle for varying the brightness of LED.
+    sleep(0.1)                           #sleep for 100m second 
+'''
+
+''' 
+print("FORWARD MOTION")
 GPIO.output(Motor1,GPIO.HIGH)
 GPIO.output(Motor2,GPIO.LOW)
 GPIO.output(Motor3,GPIO.HIGH)
  
-sleep(3)
+sleep(5)
  
-print "BACKWARD MOTION"
+print("BACKWARD MOTION")
 GPIO.output(Motor1,GPIO.LOW)
 GPIO.output(Motor2,GPIO.HIGH)
 GPIO.output(Motor3,GPIO.HIGH)
  
-sleep(3)
- 
-print "STOP"
+sleep(5)
+''' 
+print("STOP")
 GPIO.output(Motor3,GPIO.LOW)
  
 GPIO.cleanup()
